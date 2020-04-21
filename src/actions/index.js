8970d588c3d3354ef0c97a69129da01d0fe151ab
Cris <cr.ledesma@hotmail.com>
2020-04-21 16:45:20 -0300

    Added login and register without full operation

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,23 @@ export const setFavorite = (payload) => ({
   type: 'SET_FAVORITE',
   payload,
 });
+
+export const loginRequest = (payload) => ({
+  type: 'LOQIN_REQUEST',
+  payload,
+});
+
+export const logoutRequest = (payload) => ({
+  type: 'LOGOUT_REQUEST',
+  payload,
+});
+
+export const deleteFavorite = (payload) => ({
+  type: 'DELETE_FAVORITE',
+  payload,
+});
+
+export const registerRequest = (payload) => ({
+  type: 'REGISTER_REQUEST',
+  payload,
+});
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CaroulselItem';
-import useInitialState from '../hooks/useInitialState';
+// import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 const Home = ({ myList, trends, originals }) => {
@@ -14,7 +14,7 @@ const Home = ({ myList, trends, originals }) => {
       {myList.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
-            {myList.map((item) => <CarouselItem key={item.id} {...item} />)}
+            {myList.map((item) => <CarouselItem key={item.id} {...item} isList />)}
           </Carousel>
         </Categories>
       )}
